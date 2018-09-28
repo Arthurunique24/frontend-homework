03fdeb119d5f6d36983f6e1eefbfba853a5113a5
arthurunique24 <arthurunique24@gmail.com>
2018-09-28 18:35:19 +0300

    Fix func

--- source/plainify.js
+++ source/plainify.js
@@ -24,24 +24,3 @@ function plainify(obj) {
 
     return result;
 }
-
-function newPlainify(obj) {
-    const result = {};
-
-    if (!(obj instanceof Object)) {
-        return {}
-    }
-    const objEntries = Object.entries(obj);
-    let prevKey = '';
-
-    return objEntries.reduce((acc, value) => {
-        const key = prevKey === '' ? `${prevKey}${value[0]}` : `${prevKey}.${value[0]}`;
-
-        if (value[1].constructor !== Object) {
-            result[key] = value[1];
-            return acc;
-        }
-        acc[key] = reducer(key, value[1], result);
-        return acc;
-    }, {});
-}
